e488834f74d42d017d1db00ff57e9c84a8fec06e
Benjamin Riggs <deisum@gmail.com>
2014-03-27 18:06:02 -0500

    Moodle doesn't exactly LTI...

--- project/lti_views.py
+++ project/lti_views.py
@@ -7,6 +7,7 @@ __author__ = 'riggs'
 
 from datetime import datetime
 from calendar import timegm
+from uuid import uuid4
 
 from oauth2 import Error as OAuthError
 
@@ -37,6 +38,8 @@ def lti(request):
     path = request.matchdict['path']
     if path == 'launch':
         return _launch(request)
+    if path == 'assessment':
+        return _assessment(request)
     raise HTTPUnauthorized()
 
 
@@ -73,15 +76,37 @@ def _authorize_tool_provider(request):
 
     return tool_provider
 
+_session = None
 
 def _launch(request):
     tool_provider = _authorize_tool_provider(request)
+    global _session
+    _session = tool_provider
     username = tool_provider.username(default="beautiful")
     if tool_provider.is_outcome_service():
         return render_to_response("templates/lti_assessment.pt", locals(),  request)
-    uid = new_id()
     activity = activity_name(tool_provider.custom_params['custom_box_version'])
+    """
+    uid = new_id()
     fake_DB[uid] = tool_provider
-    fake_DB[tool_provider.user_id][tool_provider.context_id]\
+    fake_DB[tool_provider.tool_consumer_instance_guid]\
+           [tool_provider.user_id][tool_provider.context_id]\
            [tool_provider.resource_link_id][tool_provider.consumer_key] = uid
+    """
     return render_to_response("templates/demo.pt", locals(), request)
+
+
+def _assessment(request):
+    global _session
+    if _session is None:
+        raise HTTPBadRequest("Tool didn't launch")
+    key = _session.oauth_consumer_key
+    tool_provider = WebObToolProvider(key, _OAuth_creds[key], _session.params)
+
+    if not tool_provider.is_outcome_service():
+        raise HTTPBadRequest("Tool wasn't launched as an outcome service")
+
+    outcome_request = tool_provider.new_request()
+    outcome_request.messsage_identifier = uuid4()
+    outcome_request.post_replace_result(request.POST['score'])
+    return Response("\r\n".join([outcome_request.outcome_response.post_response, '', "Request XML:", outcome_request.generate_request_xml()]))
