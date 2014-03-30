819be9e138fa6c331746904400854c3f23e49683
Benjamin Riggs <deisum@gmail.com>
2014-03-29 20:54:14 -0500

    Lets use a working OAuth library.

---
+++ project/outcome_request.py
@@ -0,0 +1,24 @@
+"""
+Subclassing to replace failing OAuth functionality.
+"""
+
+import hashlib
+import base64
+from requests_oauthlib import OAuth1Session
+
+from ims_lti_py import OutcomeRequest
+
+
+class OutcomeRequest(OutcomeRequest):
+
+    def post_outcome_request(self):
+        '''
+        POST an OAuth signed request to the Tool Consumer.
+        '''
+        session = OAuth1Session(self.consumer_key, self.consumer_secret)
+
+        body = self.generate_request_xml()
+        headers = {'Content-Type': 'application/xml',
+                   'oauth_body_hash': base64.b64encode(hashlib.sha1(body).digest())}
+
+        response = session.post(self.lis_outcome_service_url, data=body, headers=headers)
