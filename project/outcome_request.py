230f79f5caeed3ee051b7bebd661358a607810cf
Benjamin Riggs <deisum@gmail.com>
2014-03-31 00:24:16 -0500

    UPDATE YOUR PACKAGE ON PYPI, ASSHOLE!

--- project/outcome_request.py
+++ project/outcome_request.py
@@ -28,3 +28,34 @@ class OutcomeRequestOAuthlib(OutcomeRequest):
 
         self.outcome_response = outcome_response
         return outcome_response
+
+    def generate_request_xml(self):
+        root = etree.Element('imsx_POXEnvelopeRequest', xmlns =
+        'http://www.imsglobal.org/lis/oms1p0/pox')
+
+        header = etree.SubElement(root, 'imsx_POXHeader')
+        header_info = etree.SubElement(header, 'imsx_POXRequestHeaderInfo')
+        version = etree.SubElement(header_info, 'imsx_version')
+        version.text = 'V1.0'
+        message_identifier = etree.SubElement(header_info,
+                                              'imsx_messageIdentifier')
+        message_identifier.text = self.message_identifier
+        body = etree.SubElement(root, 'imsx_POXBody')
+        request = etree.SubElement(body, '%s%s' %(self.operation,
+                                                  'Request'))
+        record = etree.SubElement(request, 'resultRecord')
+
+        guid = etree.SubElement(record, 'sourcedGUID')
+
+        sourcedid = etree.SubElement(guid, 'sourcedId')
+        sourcedid.text = self.lis_result_sourcedid
+
+        if self.score:
+            result = etree.SubElement(record, 'result')
+            result_score = etree.SubElement(result, 'resultScore')
+            language = etree.SubElement(result_score, 'language')
+            language.text = 'en'
+            text_string = etree.SubElement(result_score, 'textString')
+            text_string.text = self.score.__str__()
+
+        return etree.tostring(root, xml_declaration = True, encoding = 'utf-8')
